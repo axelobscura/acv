eda1a967ffbfaffd2039d13e42b1eba3d7506cbe
axel <axosar@gmail.com>
2023-08-26 11:46:17 -0600

    logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,23 +3,16 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        
-      </div>
-
-      <div className="relative flex place-items-center before:absolute ">
+      <div className="relative flex flex-col place-items-center before:absolute ">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
+          //className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/logo-imcyc-blanco.svg"
+          alt="Instituto Mexicano del Cemento y del Concreto A.C."
+          width={300}
           height={37}
           priority
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        
+        <p className='p-5'><small>Instituto Mexicano del Cemento y del Concreto A.C.</small></p>
       </div>
     </main>
   )
