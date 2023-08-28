09642c412e8d4f8a7edfd65437d3e663ef5e7ce8
axel <axosar@gmail.com>
2023-08-28 17:30:16 -0600

    icons

--- src/app/emisiones/page.tsx
+++ src/app/emisiones/page.tsx
@@ -1,8 +1,7 @@
 "use client"
 import { useState, useEffect } from 'react'
 import Menu from "../components/menu"
-
-
+import { ChevronRightIcon } from '@heroicons/react/24/solid'
 
 export default function Emisiones() {
     const [data, setData] = useState(null)
@@ -32,10 +31,18 @@ export default function Emisiones() {
                 <div className="basis-1/4">
                     <Menu />
                 </div>
-                <div className="basis-1/2 pl-10 pr-10">
-                    <h1 className="font-orbitron text-4xl">Emisiones de Gases de Efecto Invernadero</h1>
-                    <hr/>
-                    <p>{employee.message}</p>
+                <div className="pl-10 pr-10 bg-customVerdeDos p-10 rounded-medium">
+                    <h1 className="font-orbitron text-4xl pb-2">Emisiones de Gases de Efecto Invernadero</h1>
+                    <hr className='mb-3'/>
+                    {/*<p>{employee.message}</p>*/}
+                    <p className='flex font-orbitron text-gray-100 mt-3 mb-2'><ChevronRightIcon className="h-6 w-6 text-customVerdeUno" /> Año de declaración:</p>
+                    <input type="number" placeholder='Año de declaración' className="font-orbitron border border-gray-300 rounded-md px-4 py-2 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 w-full"></input>
+                    <p className='flex font-orbitron text-gray-500 mt-3 mb-2'><ChevronRightIcon className="h-6 w-6 text-customVerdeUno" /> Nombre común del producto:</p>
+                    <input type="text" placeholder='Nombre común del producto' className="font-orbitron border border-gray-300 rounded-md px-4 py-2 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 w-full"></input>
+                    <p className='flex font-orbitron text-gray-500 mt-3 mb-2'><ChevronRightIcon className="h-6 w-6 text-customVerdeUno" /> Producción total del producto para el año declarado:</p>
+                    <input type="number" placeholder='Producción total del producto para el año declarado' className="font-orbitron border border-gray-300 rounded-md px-4 py-2 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 w-full"></input>
+                    <p className='flex font-orbitron text-gray-500 mt-3 mb-2'><ChevronRightIcon className="h-6 w-6 text-customVerdeUno" /> Materias primas:</p>
+                    <input type="text" placeholder='Materias primas' className="font-orbitron border border-gray-300 rounded-md px-4 py-2 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 w-full"></input>
                 </div>
             </div>
         </main>
