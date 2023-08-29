66ce57f3a038042440fc225aad45c035d7b86121
axel <axosar@gmail.com>
2023-08-28 18:27:56 -0600

    display

--- src/app/emisiones/page.tsx
+++ src/app/emisiones/page.tsx
@@ -27,7 +27,7 @@ export default function Emisiones() {
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-center p-10">
-            <div className="flex flex-row w-full justify-space-evenly">
+            <div className="flex flex-col sm:flex-row w-full justify-space-evenly">
                 <div className="basis-1/4">
                     <Menu />
                 </div>
@@ -35,13 +35,11 @@ export default function Emisiones() {
                     <h1 className="flex font-orbitron text-4xl pb-2"><ArrowRightCircleIcon className="h-10 w-10 text-gray-300" /> Emisiones de Gases de Efecto Invernadero</h1>
                     <hr className='mb-3'/>
                     {/*<p>{employee.message}</p>*/}
-
                     <div className='flex justify-around mb-5'>
                         <h2 className='text-3xl font-orbitron w-full bg-customVerdeUno text-center p-3 hover:bg-customVerdeDos cursor-pointer'>Etapa A1</h2>
                         <h2 className='text-3xl font-orbitron w-full bg-customVerdeUno text-center p-3 hover:bg-customVerdeDos cursor-pointer'>Etapa A2</h2>
                         <h2 className='text-3xl font-orbitron w-full bg-customVerdeUno text-center p-3 hover:bg-customVerdeDos cursor-pointer'>Etapa A3</h2>
                     </div>
-
                     <div className='etapaA1'>
                         <h2 className='flex items-center font-orbitron text-2xl bg-customVerdeDos p-3'><ArrowSmallRightIcon className="h-5 w-5 text-gray-300 mr-1" /> Etapa A1</h2>
                         <p className='flex font-orbitron text-gray-100 mt-3 mb-2'><ChevronRightIcon className="h-6 w-6 text-gray-300" /> Año de declaración:</p>
