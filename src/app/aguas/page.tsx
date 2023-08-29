e81f339483c86e9428bbf6ca6dd18064d56fae3d
axel <axosar@gmail.com>
2023-08-29 11:17:23 -0600

    resultados

--- src/app/aguas/page.tsx
+++ src/app/aguas/page.tsx
@@ -1,3 +1,4 @@
+import { ChevronRightIcon, ArrowRightCircleIcon, ArrowSmallRightIcon, ArrowSmallLeftIcon } from '@heroicons/react/24/solid'
 import Menu from "../components/menu"
 
 export default function Aguas() {
@@ -9,14 +10,17 @@ export default function Aguas() {
         'backgroundPosition': 'center',
         'backgroundColor': '#222222',
       }}>
-        <div className="flex flex-col sm:flex-row w-full justify-space-evenly">
-          <div className="basis-1/4">
-            <Menu />
-          </div>
-          <div className="basis-1/2 pl-10 pr-10">
-            <h1 className="font-orbitron text-4xl">Consumo de Agua</h1>
-          </div>
-        </div>
+        <div className="flex flex-col sm:flex-row w-full items-center justify-space-evenly">
+                <div className="basis-1/4">
+                    <Menu />
+                </div>
+                <div className="pl-10 pr-10 p-10 rounded-2xl w-full">
+                    <h1 className="flex font-orbitron text-4xl pb-2"><ArrowRightCircleIcon className="h-10 w-10 text-gray-300" /> Consumo de Agua</h1>
+                    <hr className='mb-3'/>
+                    <p className='flex font-orbitron text-gray-100 mt-3 mb-2'><ChevronRightIcon className="h-6 w-6 text-gray-300" />Consumo total de agua para el año de declarado:</p>
+                    <input type="number" placeholder='Consumo total de agua para el año de declarado' className="font-orbitron border border-gray-300 rounded-md px-4 py-2 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 w-full"></input>
+                </div>
+            </div>
       </main>
     )
   }
